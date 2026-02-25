a367beecc277113b992f84227cebadc9c27ca80f
Romulo Rosa | FullStack | Rosadev Solutions <rosadevsolutions@gmail.com>
2026-02-25 20:17:41 -0300

    app rota configuracaoes e envio da response

--- server.js
+++ server.js
@@ -10,3 +10,15 @@ const app = express()
 //3000 - Valor da porta no localhost
 //const port - Criação da instancia da porta pra que depois possa ser acessada
 const port = 3000
+
+
+//app.get - Define uma rota http do tipo get
+//'/' - rota raiz (home do servidor)
+//(req,res) => {} - Função callback executada ao acessar a rota
+//req - request - requisição - Recebe dados do server
+//res - tesponse - resposta - Envia dados pro client
+app.get('/', (req,res) => {
+  //res.send() - Envia a resposta ao navegador
+  // Será vista ao acessar http://localhost:portaconfigurada
+  res.send("Olá Mundo!")
+})
