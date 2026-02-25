2e935c0aed1ce2494c5d659e459d52e343ddcc9f
Romulo Rosa | FullStack | Rosadev Solutions <rosadevsolutions@gmail.com>
2026-02-25 20:09:26 -0300

    variaveis importacao inicializacao e porta

---
+++ server.js
@@ -0,0 +1,12 @@
+//require('express') - importacao do framework
+//const express - variavel pra armazenar essa importação
+const express = require('express')
+
+// express() - Executando a função express
+// const app - Criação da instancia do app e assim, representação do servidor
+//Através da app vamos configurar rotas, middlewares e etc
+const app = express()
+
+//3000 - Valor da porta no localhost
+//const port - Criação da instancia da porta pra que depois possa ser acessada
+const port = 3000
